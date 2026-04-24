cda5844987cb82dede72856784b278c7cafa3eab
dengj <dengguan12@gmail.com>
2026-04-24 06:12:47 -0400

    fixed naming

---
+++ tests/test_queue.py
@@ -0,0 +1,10 @@
+def test_queue_add():
+    queue = []
+    queue.append("song1.mp3")
+    assert len(queue) == 1
+    assert queue[0] == "song1.mp3"
+
+
+def test_queue_empty():
+    queue = []
+    assert len(queue) == 0
